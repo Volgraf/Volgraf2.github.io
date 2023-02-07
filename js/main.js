fe477f93e78fda9da1a642542e37b5bbf02a59b1
Volgraf <88399388+Volgraf@users.noreply.github.com>
2023-02-07 20:45:35 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-left',
+    prevEl: '.swiper-button-right',
+  },
+});
